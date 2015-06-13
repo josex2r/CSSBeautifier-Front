4ccf5853f5db395f0c244a9f159b9187bdc677ad
josex2r <jlrepresa@gmail.com>
2015-06-13 17:04:52 +0200

    Added i18n

---
+++ app/locales/es/translations.js
@@ -0,0 +1,5 @@
+export default {
+  'label': {
+    'appTitle': 'Beauty CSS'
+  }
+};
--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,10 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.i18n = {
+    defaultLocale: 'es'
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
